1760300e16e5c48679b7c2f470e4da33f4435078
Lars Söderkvist <updates11@mac.com>
2014-01-24 00:53:10 -0600

    Add class JuryTest to helper.rb

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,33 @@
 require 'minitest/autorun'
+require_relative '../lib/environment'
 
-def assert_command_output expected, command
-  actual = `#{command}`.strip
-  assert_equal expected, actual
+class JuryTest < MiniTest::Unit::TestCase
+  def database
+    Environment.database_connection("test")
+  end
+
+  def teardown
+    database.execute("delete from songs")
+  end
+
+  def assert_command_output expected, command
+    actual = `#{command} --environment test`.strip
+    assert_equal expected, actual
+  end
+
+  def assert_in_output output, *args
+    missing_content = []
+    args.each do |argument|
+      unless output.include?(argument)
+        missing_content << argument
+      end
+    end
+    assert missing_content.empty?, "Output didn't include #{missing_content.join(", ")}:\n #{output}"
+  end
+
+  def assert_not_in_output output, *args
+    args.each do |argument|
+      assert !output.include?(argument), "Output shouldn't include #{argument}: #{output}"
+    end
+  end
 end
