12306e8b3b9d9746df144a4ff21815cbc5162d5e
Lars Söderkvist <updates11@mac.com>
2014-01-26 20:48:22 -0600

    Add test for database searching

---
+++ test/test_searching_songs.rb
@@ -0,0 +1,18 @@
+require_relative 'helper'
+
+class TestSearchingSongs < JuryTest
+  def test_search_returns_relevant_results
+    `./jury add 'Pancake Lizard' --artist 'Aphex Twin' --genre Electronic --intensity 3 --focusing 0 --environment test`
+    `./jury add 'Cantus In Memory Of Benjamin Britten' --artist 'Arvo Pärt' --genre Classical --intensity 4 --focusing 1 --environment test`
+    `./jury add 'Yègellé Tezeta (My Own Memory)' --artist 'Mulatu Astatke' --genre Ethiopian Jazz --intensity 4 --focusing 1 --environment test`
+
+    shell_output = ""
+    IO.popen('./jury search --environment test', 'r+') do |pipe|
+      pipe.puts("Memory")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_in_output shell_output, "Cantus In Memory Of Benjamin Britten", "Yègellé Tezeta (My Own Memory)"
+    assert_not_in_output shell_output, "Pancake Lizard"
+  end
+end
